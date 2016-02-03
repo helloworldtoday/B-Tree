4009fcc010d6a1d3bc0294f6180f39ab93bfeef0
helloworldtoday <helloworldtoday@users.noreply.github.com>
2016-02-02 16:40:58 -0800

    Update Utils.java

--- Utils.java
+++ Utils.java
@@ -21,8 +21,7 @@ public class Utils {
     }
   }
 	
-  public static <K extends Comparable<K>,T> String 
-    outputTree(BPlusTree<K,T> tree) {
+  public static <K extends Comparable<K>,T> String outputTree(BPlusTree<K,T> tree) {
     /* Temporary queue. */
     LinkedBlockingQueue<Node<K,T>> queue;
   
